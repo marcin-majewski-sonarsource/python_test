ce37e945fb325c63b5e35f9704e987da487b379e
Marcin Majewski <marcin.majewski@sonarsource.com>
2020-09-08 12:03:12 +0200

    * New file added

--- new_file.py
+++ new_file.py
@@ -1,6 +1,6 @@
 class A:
     def __mul__(self, other, unexpected):  # Noncompliant. Too many parameters
-        return 42
+        return 52
 
     def __add__(self):  # Noncompliant. Missing one parameter
-        return 42
+        return 52
